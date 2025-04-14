27ae1c2fe5097216b669a91eb35b138d37f17ef4
devgiants75 <devgiants75@naver.com>
2025-04-14 15:47:16 +0900

    20250414 mysql - stored procedure practice

--- g_프로그래밍/d_프로시저_예제.sql
+++ g_프로그래밍/d_프로시저_예제.sql
@@ -26,3 +26,53 @@ from
         on B.mem_id = M.mem_id
 group by M.mem_id
 order by sum(B.price * B.amount) desc;
+
+# 2. baseball_league
+use baseball_league;
+
+# 1) 변수 사용
+# : 팀 ID를 변수에 저장, 저장된 팀 ID를 사용하여 선수 조회
+set @team_id = 1; -- 변수는 실행해야 임시 공간에 저장
+
+select * from players
+where
+	team_id = @team_id;
+
+# 2. 스토어드 프로시저
+drop procedure if exists CheckPosition;
+
+delimiter $$
+
+# 프로시저에 매개변수 사용
+# : 프로시저명(in 매개변수명 데이터타입)
+
+create procedure CheckPosition(in p_player_id int)
+begin
+	# 프로시저 내의 지역 변수
+	declare v_player_position varchar(100);
+
+	select
+		# 매개변수의 값에 해당하는 선수 포지션을 조회
+        # A into B(A를 B에 넣다)
+		position into v_player_position
+		from 
+			players
+		where
+			player_id = p_player_id; # 매개변수의 id와 일치하는 선수 id의 데이터 조회
+
+		if v_player_position = '타자' then
+			select '해당 선수는 타자입니다.';
+		else
+			select '해당 선수는 타자가 아닙니다.';
+            
+		end if;
+end $$
+
+delimiter ;
+
+call CheckPosition(101);
+call CheckPosition(105);
+
+call CheckPosition(103);
+
+select * from players;
