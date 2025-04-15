77cbfbb4cc83a1766c48cd7eadae2caa4f33e16d
devgiants75 <devgiants75@naver.com>
2025-04-15 15:50:26 +0900

    20250414 mysql - 정규화

--- j_정규화/제2정규화.sql
+++ j_정규화/제2정규화.sql
@@ -21,13 +21,48 @@
 # 1. 1NF를 만족: 컬럼에 중복 데이터 없이, 원자값으로 구성
 # 2. 기본키가 복합키일 때, 모든 비기본 속성이 기본키 전체에 의존!해야 함
 
+-- 부서 테이블
+create table departments (
+	department_id varchar(50) primary key, -- 기본키
+    location varchar(50), -- 부서 위치 
+    supervisor_id varchar(50) -- 부서장 ID
+);
 
+-- 직원 테이블
+create table employees (
+	employee_id varchar(10) primary key, -- 기본 키
+    department_id varchar(50), -- 직원이 속한 부서
+    
+    foreign key (department_id) references departments(department_id)
+);
 
+# 데이터 삽입 #
+insert into departments
+values
+	('HR', 'seoul', 's1'),
+	('SALES', 'busan', 's2'); -- location과 supervisor_id 속성이 기본 키에 완전 종속
+    
+insert into employees
+values
+	('e1', 'SALES'),
+	('e2', 'HR'),
+	('e3', 'SALES'),
+	('e4', 'HR');
+    
+# 1) 1NF 상태: 각 컬럼에 원자값만 존재
+# 2) 기본키 employee_id
+# 3) 직원의 기본키를 제외하고는 기본 키에 완전 종속
 
+# >> 두 테이블 모두 2NF를 만족
 
+### 제2정규형을 위반한 테이블 ###
+create table wrong_2nf (
+	employee_id varchar(10), -- 직원 고유 식별자
+    department varchar(50), -- 부서명
+    location varchar(50), -- 부서 위치 (문제 발생!, 2NF 위반: department 만으로 결정!)
+    
+    primary key (employee_id, deparment) -- 복합키
+);
 
-
-
-
-
-
+# >> 부분 종속이 발생하면 테이블을 분리하여 해결하는 것이 2NF를 지키는 방법
+#    : 테이블을 나누어 각각의 비기본 속성이 완전 종속이 되도록 설계
