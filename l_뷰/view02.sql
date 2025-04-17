7596440256b54208228181f558fc6980b0a6baf6
devgiants75 <devgiants75@naver.com>
2025-04-17 10:22:36 +0900

    20250417 - mysql view

--- l_뷰/view02.sql
+++ l_뷰/view02.sql
@@ -35,13 +35,60 @@ drop view v_view_test01;
 
 # select * from v_view_test01;
 
+# cf) 테이블 생성 시 '존재하는 테이블일 경우 생성하지 않는' 옵션
+# create table 'if not exists' 테이블명 ();
 
+# >> 뷰 존재 여부에 관계없이 생성
+#    : create(생성하다) or replace(대체하다)
+#    : 기존에 뷰가 있으면 덮어쓰고, 없으면 새로 생성
+#    - 실습 및 반복작업 시 유용
 
+create or replace view v_view_test02
+as
+	select mem_id, mem_name from member;
 
+select * from v_view_test02;
 
+### 뷰 정보 확인 ###
+# describe (desc): 열(컬럼)의 정보 확인 / PK, 제약조건은 확인 불가!
+describe v_view_test02;
 
+# show create view 뷰이름;: 생성된 뷰의 sql 코드를 확인
+show create view v_view_test02;
 
+### 뷰를 통한 데이터 수정/삭제 ###
+# : 뷰를 통해서 원본 테이블의 데이터를 수정
+# >> 단! 뷰에 포함되지 않은 컬럼에 의한 제약조건에 따라 제한될 수 있음
+select * from v_member;
 
+# 뷰를 사용한 데이터 수정 #
+update v_member 
+set
+	addr = '부산'
+where mem_id = 'BLK';
 
+select * from v_member;
 
+select * from member; # 뷰의 데이터 조작이 실제 테이블에도 적용
 
+-- insert into v_member (mem_id, mem_name, addr)
+-- values 
+-- 	('IVE', '아이브', '경기');
+# >> v_member가 참조하는 원본 테이블의 NOT NULL 컬럼이 뷰에 포함되어 있지 않으면 삽입 불가!
+
+create view v_height167
+as
+	select * from member
+    where 
+		height >= 167;
+
+select * from v_height167;
+
+# 뷰를 사용한 데이터 삭제 #
+delete from v_height167 
+	where height = 168;
+    
+    # >> 원본 테이블의 기준(참조 테이블 연결)과 충돌되어 삭제 X
+
+check table v_height167;
+# : 뷰의 상태 확인 가능
---
+++ l_뷰/view_practice.sql
@@ -0,0 +1,27 @@
+### 'l_뷰' 폴더 >>> 'view_practice' 파일 ###
+
+### 뷰 연습 문제 ###
+create database school;
+use school;
+
+-- students 테이블 생성 --
+# student_id: 정수, 기본키
+# name: 문자열(50),
+# age: 정수
+# major: 문자열(50)
+
+-- courses 테이블 생성 --
+# cours_id: 정수, 기본키
+# course_name: 문자열(100)
+# instructor: 문자열(100)
+# credit_hours: 정수
+
+-- student_courses 테이블 --
+# student_id: 정수
+# course_id: 정수
+# >> 위의 두 컬럼을 복합키로 설정(기본키)
+# >> 각 컬럼은 students, courses 테이블에서 참조
+
+
+
+
